853c6872f8ff130a9d449937426d46f2ffd7fc1f
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-15 10:18:18 -0700

    Completed Assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    var cat = [...cats, name];
+
+    return cat;
+}
+
+function prependCat(name){
+    var cat = [name,...cats];
+
+    return cat;
+}
+
+function removeLastCat(){
+    var cat = [... cats];
+    cat.pop();
+
+    return cat;
+}
+
+function removeFirstCat(){
+
+    return cats.slice(1);
+
+}
